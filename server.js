bcdcb00f6c836fff449fe60e8988e9811d8d23c4
Carmen Realpe <carmenrealpe@yahoo.es>
2021-10-15 20:41:17 -0500

    token server.js

--- server.js
+++ server.js
@@ -2,15 +2,33 @@ import Express from 'express';
 import Cors from 'cors';
 import dotenv from 'dotenv';
 import { conectarBD } from './db/db.js';
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
 import rutasVentas from './views/ventas/rutas.js';
 import rutasProductos from './views/productos/rutas.js';
 import rutasUsuarios from './views/usuarios/rutas.js';
+
 dotenv.config({ path: './.env' });
 
 const app = Express();
 
 app.use(Express.json());
 app.use(Cors());
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://misiontic3.us.auth0.com/.well-known/jwks.json'
+}),
+audience: 'api-ventas-mintic',
+issuer: 'https://misiontic3.us.auth0.com/',
+algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
+
 app.use(rutasVentas);
 app.use(rutasProductos);
 app.use(rutasUsuarios);
